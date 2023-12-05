5943b5eb528cd43d9f504cd88e64ad774b4c730a
kmille <github@androidloves.me>
2023-12-05 12:40:51 +0100

    Run tests as root

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ poetry install
 # run by git hooks
 poetry run flake8 --ignore=E501 --show-source --statistics riseup_vpn_configurator
 poetry run mypy riseup_vpn_configurator
-poetry run pytest -v -s tests
+sudo poetry run pytest -v -s tests
 
 # update version
 poetry version patch
